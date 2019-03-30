2c55a459c008818aeee89a37525a81537d82bdcc
BitBD <tanvirtex@gmail.com>
2019-03-31 05:21:38 +0600

    make executable

--- allset.sh
+++ allset.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo -e "Ruby 2.3.0 stable install with gemset"
 sudo apt install gnupg2
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
